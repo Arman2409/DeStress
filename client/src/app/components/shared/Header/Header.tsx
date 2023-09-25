54bae77d1071563c2f419fa5b1197c6d10840eb8
Arman Ghazaryan <arman.ghazaryan@hylink.am>
2023-09-25 21:54:15 +0400

    Project started

---
+++ client/src/app/components/shared/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from "../../../../styles/components/shared/header.module.scss"
+
+const Header = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
