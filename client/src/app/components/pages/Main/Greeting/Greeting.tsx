501a64e40bc4b59bc5dc25e9135f92d6f5b6bb8f
Arman Ghazaryan <arman.ghazaryan@hylink.am>
2023-09-26 17:35:16 +0400

    Logo animation bug fixed

--- client/src/app/components/pages/Main/Greeting/Greeting.tsx
+++ client/src/app/components/pages/Main/Greeting/Greeting.tsx
@@ -3,13 +3,20 @@
 import { motion } from "framer-motion"
 
 import styles from "../../../../../styles/components/greeting.module.scss"
+import { logoVariants } from "./utils/variants"
 
 const Greeting = () => {
    return (
       <div className={styles.main}>
-       <img src="/logo_title.png" />
+         <motion.img
+            src="/logo_title.png"
+            initial="initial"
+            animate="animate"
+            whileHover="hover"
+            variants={logoVariants}
+         />
       </div>
-   )  
+   )
 }
 
 export default Greeting;
