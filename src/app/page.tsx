0c1c2f9f9e92209ecbe74ce1154f5836e6ca9cce
Arman Ghazaryan <arman.ghazaryan@hylink.am>
2023-10-28 03:17:06 +0400

    Ball bouncing added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 import styles from "../styles/page.module.scss"
 import GameTiles from "./components/GameTiles/GameTiles"
@@ -7,13 +7,37 @@ import Greeting from "./components/Greeting/Greeting"
 import Loading from "./globals/components/Loading/Loading"
 
 export default function Home() {
-  const [loading, setLoading] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false);
+  const circle = useRef<any>(null);
+  const isBouncing = useRef<boolean>(false);
+
+  useEffect(() => {
+    document.addEventListener('mousemove', (event: MouseEvent) => {
+      if (isBouncing.current) return;
+      let left = event.clientX;
+      let top = event.clientY;
+      if (event.movementX !== 0) {
+        isBouncing.current = true;
+        circle.current.style.top = document.body.clientHeight - 50 + "px";
+        circle.current.style.left = left - (Math.abs(event.movementX) / 2) + "px";
+        setTimeout(() => {
+          circle.current.style.left = left - 25 + 'px';
+          circle.current.style.top = top - 25 + 'px';
+          setTimeout(() => isBouncing.current = false,1000)
+        }, 1000);
+        return;
+      }
+      circle.current.style.left = left + 'px';
+      circle.current.style.top = top + 'px';
+    });
+  }, [])
 
   return (
-    <main className={styles.main}>
-      {loading && <Loading/>}
+    <main className={styles.main} >
+      <div id="circle" ref={circle} className={styles.circle} />
+      {loading && <Loading />}
       <Greeting />
-      <GameTiles choseGame={() => setLoading(true)}/>
+      <GameTiles choseGame={() => setLoading(true)} />
     </main>
   )
 }
